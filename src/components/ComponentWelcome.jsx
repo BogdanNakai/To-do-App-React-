40c78e7bcefaced6b49c2f69a78b0cd94430f3e6
Bogdan <nakaibogdan@gmail.com>
2026-04-23 11:43:13 +0300

    feat: Add display logic for the "Welcome" block

--- src/components/ComponentWelcome.jsx
+++ src/components/ComponentWelcome.jsx
@@ -1,18 +1,21 @@
+import { useState } from 'react';
 import '../scss/registration.scss'
 
+
+
 const ComponentWelcome = () => {
+
+	const [welcomePosition, setWelcomePosition] = useState('signup');
+	const isSignIn = welcomePosition === "signin"
+
 	return (
-		<div className="registration--welcome welcome welcome_right">
+		<div className={`registration--welcome welcome welcome_${welcomePosition}`}>
 			<div className="welcome--content">
-				<h1 className="welcome--title">Hello, Welcome!</h1>
-				<h1 className="welcome--title">Welcome Back!</h1>
-				<p className="welcome--text">Don’t have on account</p>
+				<h1 className="welcome--title">{isSignIn ? 'Hello, Welcome!' : "Welcome Back!"}</h1>
+				<p className="welcome--text">Don`t have on account?</p>
 				<div className="welcome--box-button">
-					<button className="welcome--button button">
-						Registration
-					</button>
-					<button className="welcome--button button">
-						Login
+					<button onClick={() => isSignIn ? setWelcomePosition('signup') : setWelcomePosition('signin')} className="welcome--button button">
+						{isSignIn ? "Registration" : 'Login'}
 					</button>
 				</div>
 			</div>
