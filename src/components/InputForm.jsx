4d6dc5b5f66b562ad7375468903f86aee18bd9a8
Bogdan <nakaibogdan@gmail.com>
2026-04-24 15:38:22 +0300

    feat: add validation to registration fields

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,13 +1,16 @@
 
 
-const InputForm = ({ type, name, id, icon, placeholder }) => {
+
+const InputForm = ({ type, name, id, icon, placeholder, onChangeText, errors}) => {
+
 	return (
 		<div className="form--box-input">
 			<div className="form--content-input">
-				<input type={type} name={name} id={id} placeholder={placeholder} className="form--input"/>
+				<input type={type} name={name} id={id} placeholder={placeholder} onChange={onChangeText} className="form--input" />
 				<div className="form--input-icon">
 					<img src={icon} alt={icon} />
 				</div>
+				{errors[name] ? <span className='form--error-message'> {errors[name].message}</span> : null}
 			</div>
 			{name === 'passwordSingIn' && <a href="" className="form--forgot-password">Forgot Password!</a>}
 		</div>
--- src/components/SingUp.jsx
+++ src/components/SingUp.jsx
@@ -1,3 +1,4 @@
+import { Controller, useForm } from "react-hook-form";
 import ButtonForm from "./ButtonForm";
 import Social from "./Social";
 import user from '../assets/icon_user.svg'
@@ -6,16 +7,84 @@ import email from '../assets/icon_email.svg'
 import '../scss/forms.scss'
 import InputForm from "./InputForm";
 
+
 const SingUp = () => {
+	const {
+		control,
+		handleSubmit,
+		message,
+		formState: { errors },
+	} = useForm()
+
+	const onSubmit = (data) => console.log(data)
+
 	return (
 		<>
 			<div className="registration--form form">
-				<form action="" className="form--signin">
+				<form noValidate required action="" onSubmit={handleSubmit(onSubmit)} className="form--signin">
 					<h2 className="form--title">Registration</h2>
 					<div className="form--info-input">
-						<InputForm type='text' name='user' id='user' icon={user} placeholder={'Username'} />
-						<InputForm type='email' name='email' id='email' icon={email} placeholder={'Email'} />
-						<InputForm type='password' name='passwordSingUp' id='passwordSingUp' icon={security} placeholder={'Password'} />
+						<Controller
+							control={control}
+							name='user'
+							rules={
+								{
+									required: 'Enter your Name',
+								}
+							}
+							render={({ field: { onChange, onBlur, name } }) => <InputForm type='text'
+								name={name}
+								id='user'
+								icon={user}
+								placeholder={'Username'}
+								onChangeText={onChange}
+								errors={errors}
+							/>}
+
+						/>
+						<Controller
+							control={control}
+							name='email'
+							rules={
+								{
+									required: 'Enter your email',
+									pattern: {
+										value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+										message: 'Incorrect e-mail address. Try again.'
+									}
+								}
+							}
+							render={({ field: { onChange, onBlur, name } }) => <InputForm type='email'
+								name={name}
+								id='email'
+								icon={email}
+								placeholder={'Email'}
+								onChangeText={onChange}
+								errors={errors}
+							/>}
+						/>
+						<Controller
+							control={control}
+							name='passwordSingUp'
+							rules={
+								{
+									required: 'Enter your password',
+									minLength: {
+										value: 6,
+										message: 'The password must contain a minimum of 6 characters'
+									}
+								}
+							}
+							render={({ field: { onChange, onBlur, value, name } }) => <InputForm type='password'
+								name={name}
+								id='passwordSingUp'
+								icon={security}
+								placeholder={'Password'}
+								onChangeText={onChange}
+								errors={errors}
+							/>}
+
+						/>
 					</div>
 					<div className="form--footer">
 						<ButtonForm textButton='Registration' />
