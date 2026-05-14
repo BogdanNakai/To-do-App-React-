37225e26a3802cbe1e1f50f62858189fe8f305fa
Bogdan <nakaibogdan@gmail.com>
2026-05-14 10:56:53 +0300

    feat: add a specific task page with the possibility of editing the status

---
+++ src/components/ItemTask/ItemTask.jsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import './ItemTask.scss'
+
+const ItemTask = () => {
+	const navigate = useNavigate();
+	const { userId, taskId } = useParams();
+	const [tasks, setTasks] = useState(() => {
+		const saveData = JSON.parse(localStorage.getItem('users'))
+
+		if (saveData) {
+			return saveData;
+		}
+	});
+
+	const task = tasks.find((e) => e.id === userId).tasks.find((e) => e.id === taskId)
+	const [status, setStatus] = useState((() => {
+		if (task.done) {
+			return 'Complete'
+		} else {
+			return 'Incomplete'
+		}
+	}))
+
+	useEffect(() => {
+		localStorage.setItem('users', JSON.stringify(tasks))
+	}, [tasks, status])
+
+	const handleExit = () => {
+		navigate(`/todolist/${userId}`, { replace: true })
+	}
+
+	const replaceStatus = () => {
+		const newDone = tasks.map((e) => {
+			if (e.id === userId) {
+				return {
+					...e,
+					tasks: e.tasks.map((t) => {
+						if (t.id === taskId) {
+							return { ...t, done: !t.done }
+						}
+						return t
+					})
+
+				}
+			}
+			return e
+		})
+		setTasks(newDone)
+		setStatus(task.done ? 'Incomplete' : 'Complete')
+	}
+
+
+	return (
+		<div className="page-task">
+			<div className="page-task--container">
+				<div className="page-task--body">
+					<h2 className="page-task--title">
+						{task.title}
+					</h2>
+					<p className={`page-task--status page-task--status_${status}`}>
+						Status: <span>{status}</span>
+					</p>
+					<div className="page-task--box-button-action">
+						<button onClick={replaceStatus} className="page-task--button page-task--button-status">Status</button>
+						<button onClick={handleExit} className="page-task--button page-task--button-exit">Exit</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+};
+
+export default ItemTask
--- src/pages/AppComponents.jsx
+++ src/pages/AppComponents.jsx
@@ -5,6 +5,8 @@ import Loading from "../components/Loading/Loading";
 const Login = lazy(() => import("./Login"))
 const Registration = lazy(() => import("./Registration"))
 const ToDoList = lazy(() => import("./ToDoList"))
+const NotFound = lazy(() => import("./NotFound"))
+const ItemTask = lazy(() => import("../components/ItemTask/ItemTask"))
 
 const AppComponents = () => {
 	return (
@@ -14,6 +16,8 @@ const AppComponents = () => {
 				<Route path="/registration" element={<Registration/>} /> 
 				<Route path="/login" element={<Login />} />
 				<Route path="/todolist/:id" element={<ToDoList />} />
+				<Route path="/todolist/:userId/task/:taskId" element={< ItemTask />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</Suspense>
 	)
