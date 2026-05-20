8e517f6b6e7caf6a7051cdff7a03043c83d42543
Bogdan <nakaibogdan@gmail.com>
2026-05-20 12:34:09 +0300

    pref: add useMemo to input search

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,12 +1,15 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 
 const useFilter = (filterIsDone, userTasks) => {
 
 	const [searchQuery, setSearchQuery] = useState('')
 
-	const clearsearchQuery = searchQuery.trim().toLocaleLowerCase();
 	const filteredByDone = filterIsDone === 'done' ? userTasks.filter(({ done }) => done) : filterIsDone === 'not-done' ? userTasks.filter(({ done }) => !done) : userTasks;;
-	const filteredTasks = clearsearchQuery.length > 0 ? filteredByDone.filter(({ title }) => title.toLocaleLowerCase().includes(clearsearchQuery)) : filteredByDone;
+	const filteredTasks = useMemo(() => {
+		const clearsearchQuery = searchQuery.trim().toLocaleLowerCase();
+
+		return clearsearchQuery.length > 0 ? filteredByDone.filter(({ title }) => title.toLocaleLowerCase().includes(clearsearchQuery)) : filteredByDone;
+	})
 
 	return {
 		searchQuery,
